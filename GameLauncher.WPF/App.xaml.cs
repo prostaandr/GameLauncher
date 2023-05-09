6b67d13ec2c3a0cf08391343d45e1e32f02d9cf0
Prostaandr <prostaandr@gmail.com>
2023-05-10 01:47:54 +0300

    Добавление контекста и ссылки подключения

--- GameLauncher.WPF/App.xaml.cs
+++ GameLauncher.WPF/App.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using GameLauncher.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +17,23 @@ namespace GameLauncher.WPF
     /// </summary>
     public partial class App : Application
     {
+        [STAThread]
+        static void Main()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            var config = builder.Build();
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<GameLauncherContext>();
+            var options = optionsBuilder
+                .UseSqlServer(connectionString)
+                .Options;
+
+            App app = new App();
+            MainWindow window = new MainWindow();
+            app.Run(window);
+        }
     }
 }
