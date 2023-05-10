6095c6d74da492c6aa031113c6b80a7b165499ae
Prostaandr <prostaandr@gmail.com>
2023-05-11 00:52:37 +0300

    Добавление базового репозитория

---
+++ GameLauncher.Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLauncher.Data.Interfaces
+{
+    internal interface IBaseRepository<T>
+    {
+        Task Create(T Entity);
+        Task<T> Get(int id);
+        Task<List<T>> GetAll();
+        Task Update(T Entity);
+        Task Delete(T Entity);
+    }
+}
