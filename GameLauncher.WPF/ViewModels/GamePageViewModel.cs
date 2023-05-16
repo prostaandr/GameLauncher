56700680bfacf8db7acbfea24dba10ecc3834e0a
Prostaandr <prostaandr@gmail.com>
2023-05-16 08:07:23 +0300

    Переезд на MVVM

---
+++ GameLauncher.WPF/ViewModels/GamePageViewModel.cs
@@ -0,0 +1,165 @@
+﻿using GameLauncher.Model;
+using GameLauncher.Service.Interfaces;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GameLauncher.WPF.ViewModels
+{
+    public class GamePageViewModel : BaseViewModel
+    {
+        private IApplicationService _applicationService;
+        
+        private Application _application;
+        public Application Application
+        {
+            get => _application;
+            set
+            {
+                if (value == _application) return;
+                _application = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _reviewsPersent;
+        public string ReviewsPersent
+        {
+            get => _reviewsPersent;
+            set
+            {
+                if (value == _reviewsPersent) return;
+                _reviewsPersent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _genres;
+        public string Genres
+        {
+            get => _genres;
+            set
+            {
+                if (value == _genres) return;
+                _genres = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _features;
+        public string Features
+        {
+            get => _features;
+            set
+            {
+                if (value == _features) return;
+                _features = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _languages;
+        public string Languages
+        {
+            get => _languages;
+            set
+            {
+                if (value == _languages) return;
+                _languages = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<SystemRequirementsBlock> _recSystemRequirements;
+        public ObservableCollection<SystemRequirementsBlock> RecSystemRequirements
+        {
+            get => _recSystemRequirements;
+            set
+            {
+                if (Equals(value, _recSystemRequirements)) return;
+                _recSystemRequirements = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<SystemRequirementsBlock> _minSystemRequirements;
+        public ObservableCollection<SystemRequirementsBlock> MinSystemRequirements
+        {
+            get => _recSystemRequirements;
+            set
+            {
+                if (Equals(value, _recSystemRequirements)) return;
+                _recSystemRequirements = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public GamePageViewModel()
+        {
+            _applicationService = App.serviceProvider.GetService<IApplicationService>();
+
+            InitializeAsync();
+        }
+
+        private async Task InitializeAsync()
+        {
+            Application = await _applicationService.GetApplication(3);
+
+            ReviewsPersent = (await _applicationService.GetReviewsPersent(3)).ToString();
+
+            Genres = String.Join(", ", Application.Genres.Select(g => g.Name));
+            Features = String.Join(", ", Application.Features.Select(f => f.Name));
+            Languages = String.Join(", ", Application.Languages.Select(l => l.Name));
+
+            RecSystemRequirements = FillSystemRequirements(Application.RecommendedSystemRequirements);
+            MinSystemRequirements = FillSystemRequirements(Application.MinimumSystemRequirements);
+        }
+
+        private ObservableCollection<SystemRequirementsBlock> FillSystemRequirements(SystemRequirements systemRequirements)
+        {
+            if (systemRequirements == null) return null;
+
+            ObservableCollection<SystemRequirementsBlock> list = new ObservableCollection<SystemRequirementsBlock>();
+
+            if (systemRequirements.OS != null && systemRequirements.OS != "")
+                list.Add(new SystemRequirementsBlock { Title = "ОС: ", Value = systemRequirements.OS });
+
+            if (systemRequirements.Processor != null && systemRequirements.Processor != "")
+                list.Add(new SystemRequirementsBlock { Title = "Процессор: ", Value = systemRequirements.Processor });
+
+            if (systemRequirements.Memory != null && systemRequirements.Memory != "")
+                list.Add(new SystemRequirementsBlock { Title = "Память: ", Value = systemRequirements.Memory });
+
+            if (systemRequirements.Graphics != null && systemRequirements.Graphics != "")
+                list.Add(new SystemRequirementsBlock { Title = "Графика: ", Value = systemRequirements.Graphics });
+
+            if (systemRequirements.DirectX != null && systemRequirements.DirectX != "")
+                list.Add(new SystemRequirementsBlock { Title = "DirectX: ", Value = systemRequirements.DirectX });
+
+            if (systemRequirements.Network != null && systemRequirements.Network != "")
+                list.Add(new SystemRequirementsBlock { Title = "Сеть: ", Value = systemRequirements.Network });
+
+            if (systemRequirements.Storage != null && systemRequirements.Storage != "")
+                list.Add(new SystemRequirementsBlock { Title = "Место на диске: ", Value = systemRequirements.Storage });
+
+            return list;
+        }
+
+        public class SystemRequirementsBlock : BaseViewModel
+        {
+            public string Title { get; set; }
+            public string Value { get; set; }
+
+        }
+    }
+}
+    
--- GameLauncher.WPF/Views/GamePage.xaml.cs
+++ GameLauncher.WPF/Views/GamePage.xaml.cs
@@ -3,6 +3,7 @@ using GameLauncher.Data.Interfaces;
 using GameLauncher.Data.Repositories;
 using GameLauncher.Model;
 using GameLauncher.Service.Interfaces;
+using GameLauncher.WPF.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -29,74 +30,16 @@ namespace GameLauncher.WPF.Views
     /// </summary>
     public partial class GamePage : Window
     {
-        IApplicationService service;
-        Model.Application application;
-
         public GamePage()
         {
             InitializeComponent();
 
-            TestView();
+            DataContext = new GamePageViewModel();
         }
 
-        public async Task TestView()
-        {
-            service = App.serviceProvider.GetService<IApplicationService>();
-
-            application = await service.GetApplication(1);
-
-            for (int i = 0; i < application.Medias.Count; i++)
-            {
-                mediaListBox.Items.Add(new Image() { Source = new BitmapImage(new Uri(application.Medias[i].Url)), Stretch = Stretch.Uniform, StretchDirection = StretchDirection.DownOnly, Height = 80});
-            }
-            mediaListBox.SelectedIndex = 0;
-
-            gameNameTextBlock.Text = application.Name;
-            releaseDateTextBlock.Text = application.ReleaseDate.ToShortDateString().ToString();
-            shortDescriptionTextBlock.Text = application.Description;
-            posterImage.Source = new BitmapImage(new Uri(application.PosterUrl));
-            develoverTextBlock.Text = application.Developer.Name;
-            publisherTextBlock.Text = application.Publisher.Name;
-            priceTextBlock.Text = application.Price.ToString();
-
-            var persent = await service.GetReviewsPersent(application.Id);
-            reviewsPersentTextBlock.Text = persent.ToString();
-
-            genresTextBlock.Text = String.Join(", ", application.Genres.Select(a => a.Name));
-            featuresTextBlock.Text = String.Join(", ", application.Features.Select(a => a.Name));
-            languagesTextBlock.Text = String.Join(", ", application.Languages.Select(a => a.Name));
-
-            SetElementToListBox(minSystemRequirementsListBox, "ОС: ", application.MinimumSystemRequirements.OS);
-            SetElementToListBox(minSystemRequirementsListBox, "Процессор: ", application.MinimumSystemRequirements.Processor);
-            SetElementToListBox(minSystemRequirementsListBox, "Память: ", application.MinimumSystemRequirements.Memory);
-            SetElementToListBox(minSystemRequirementsListBox, "Графика: ", application.MinimumSystemRequirements.Graphics);
-            SetElementToListBox(minSystemRequirementsListBox, "DirectX: ", application.MinimumSystemRequirements.DirectX);
-            SetElementToListBox(minSystemRequirementsListBox, "Сеть: ", application.MinimumSystemRequirements.Network);
-            SetElementToListBox(minSystemRequirementsListBox, "Место на диске: ", application.MinimumSystemRequirements.Storage);
-
-            SetElementToListBox(recSystemRequirementsListBox, "ОС: ", application.RecommendedSystemRequirements.OS);
-            SetElementToListBox(recSystemRequirementsListBox, "Процессор: ", application.RecommendedSystemRequirements.Processor);
-            SetElementToListBox(recSystemRequirementsListBox, "Память: ", application.RecommendedSystemRequirements.Memory);
-            SetElementToListBox(recSystemRequirementsListBox, "Графика: ", application.RecommendedSystemRequirements.Graphics);
-            SetElementToListBox(recSystemRequirementsListBox, "DirectX: ", application.RecommendedSystemRequirements.DirectX);
-            SetElementToListBox(recSystemRequirementsListBox, "Сеть: ", application.RecommendedSystemRequirements.Network);
-            SetElementToListBox(recSystemRequirementsListBox, "Место на диске: ", application.RecommendedSystemRequirements.Storage);
-
-            reviewsListBox.ItemsSource = application.Reviews;
-
-        }
-
-        public void SetElementToListBox(ListBox listBox, string title, string value)
-        {
-            if (value != null && value != "")
-            {
-                listBox.Items.Add(title + value);
-            }
-        }
-
-        private void mediaListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            mediaImage.Source = new BitmapImage(new Uri(application.Medias[mediaListBox.SelectedIndex].Url));
-        }
+        //private void mediaListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    mediaImage.Source = new BitmapImage(new Uri(Medias[mediaListBox.SelectedIndex].Url));
+        //}
     }
 }
